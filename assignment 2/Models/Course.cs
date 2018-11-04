0e1d01e1c1d7e4da573df8aebaca6592e6953749
Shahzaib Malik <smalik51@my.centennialcollege.ca>
2018-11-03 23:42:54 -0400

    uploaded on beanstalk

---
+++ assignment 2/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace assignment2.Models
+{
+    public partial class Course
+    {
+        public string CourseCode { get; set; }
+        public string StudentId { get; set; }
+        public string EmpId { get; set; }
+        public string CourseTitle { get; set; }
+
+        public Staff Emp { get; set; }
+        public Student Student { get; set; }
+    }
+}
---
+++ assignment 2/Models/Staff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace assignment2.Models
+{
+    public partial class Staff
+    {
+        public Staff()
+        {
+            Comment = new HashSet<Comment>();
+            Course = new HashSet<Course>();
+        }
+
+        public string EmpId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+        public string Department { get; set; }
+
+        public ICollection<Comment> Comment { get; set; }
+        public ICollection<Course> Course { get; set; }
+    }
+}
